074781f68c25c1608d811b676b579a2e00a6df9f
BuildTools <unconfigured@null.spigotmc.org>
2024-02-22 22:13:20 +0900

    초기 커밋.

---
+++ PickRandomCards/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace PickRandomCards
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //CardPicker card_picker = new CardPicker();
+            
+            Console.Write("카드를 몇 장 뽑을 건가요?");
+            string s_line = Console.ReadLine();
+
+           if(int.TryParse(s_line, out int numberOfCards))
+           {
+                foreach (string card in CardPicker.PickSomCards(numberOfCards))
+                {
+                    Console.WriteLine(card);
+                }
+            }
+           else
+           {
+                Console.WriteLine("Please enter a valid number");
+           }
+
+   
+
+        }
+    }
+}
